4f3f7ffc4f349b9a4b4567c85ffc00c00b66d3ef
t-uddin <uddint@hotmail.com>
2022-08-30 20:33:02 +0100

    fix: updated CORS headers

--- app/run.py
+++ app/run.py
@@ -5,9 +5,9 @@ app = create_app()
 
 @app.after_request
 def after_request(response):
-    # response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', '*')
     # response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    # response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
 
