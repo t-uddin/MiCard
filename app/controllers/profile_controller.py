61db4c9e94cb3bc30870dc5d84c414a8154adfd9
t-uddin <uddint@hotmail.com>
2022-09-07 03:11:48 +0100

    feat: register profile

--- app/controllers/profile_controller.py
+++ app/controllers/profile_controller.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for
 from models.profile import Profile
 from flask_login import login_required, current_user
+from forms import CreateProfileForm
+
 
 profile_bp = Blueprint('profile_bp', __name__)
 
@@ -111,3 +113,128 @@ def get_edit_profile():
     else:
         profile = Profile.get(account_id)
         return render_template("edit_profile.html", profile=profile)
+
+
+
+@profile_bp.route('/profile-create/', methods=['GET', 'POST'])
+@login_required
+def get_create_profile():
+    account_id = current_user.id
+
+    if request.method == "POST":
+        # get form data
+        bio = (request.form.get("new_bio")).strip()
+        work_email = request.form.get("new_email")
+        job_title = request.form.get("new_title")
+        phone = request.form.get("new_phone")
+        working_hours = request.form.get("new_hours")
+        location = request.form.get("new_location")
+        field = request.form.get("new_field")
+        registration = request.form.get("new_registration")
+        years_experience = request.form.get("new_years")
+        consultation_fee = request.form.get("new_fee")
+        specialisms = request.form.getlist("new_specialisms")
+        treatments = request.form.getlist("new_treatments")
+        certifications = request.form.getlist("new_certifications")
+        education = request.form.getlist("new_education")
+        interests = request.form.getlist("new_interests")
+
+        # remove empty list items
+        specialisms = list(filter(len, specialisms))
+        treatments = list(filter(len, treatments))
+        certifications = list(filter(len, certifications))
+        education = list(filter(len, education))
+        interests = list(filter(len, interests))
+
+        # create and save profile object
+
+        profile = Profile(
+            account=account_id,
+            bio=bio,
+            work_email=work_email,
+            job_title=job_title,
+            phone=phone,
+            working_hours=working_hours,
+            location=location,
+            field=field,
+            registration=registration,
+            years_experience=years_experience,
+            consultation_fee=consultation_fee,
+            specialisms=specialisms,
+            treatments=treatments,
+            certifications=certifications,
+            education=education,
+            interests=interests
+        )
+
+        print(profile)
+        # update user
+        profile.save()
+
+        # redirect to profile page
+        return redirect("/profile")
+
+    else:
+        return redirect(url_for('main.render_create_avatar'))
+
+
+@profile_bp.route('/profile-register/', methods=['GET', 'POST'])
+@login_required
+def get_register_profile():
+    account_id = current_user.id
+    form = CreateProfileForm()
+
+    if request.method == "POST":
+
+        if form.validate_on_submit():
+            # gather form list data
+            specialisms = request.form.getlist("specialisms")
+            treatments = request.form.getlist("treatments")
+            certifications = request.form.getlist("certifications")
+            education = request.form.getlist("education")
+            interests = request.form.getlist("interests")
+
+            # remove empty list items
+            specialisms = list(filter(len, specialisms))
+            treatments = list(filter(len, treatments))
+            certifications = list(filter(len, certifications))
+            education = list(filter(len, education))
+            interests = list(filter(len, interests))
+
+            print(specialisms)
+
+            # save form data to a new profile object
+            profile = Profile(
+                account=account_id,
+                bio=form.bio.data.strip(),
+                work_email=form.email.data,
+                job_title=form.job.data,
+                phone=form.phone.data,
+                working_hours=form.hours.data,
+                location=form.location.data,
+                field=form.field.data,
+                registration=form.registration.data,
+                years_experience=form.years.data,
+                consultation_fee=str(form.fee.data),
+                # specialisms=form.specialisms.data,
+                specialisms=specialisms,
+                treatments=treatments,
+                certifications=certifications,
+                education=education,
+                interests=interests
+            )
+
+            print(profile)
+            # update user
+            profile.save()
+
+            # redirect to create avatar page
+            return redirect(url_for('main.render_create_avatar'))
+
+        else:
+            print("error in form")
+
+    else:
+        return render_template(url_for('main.render_register_profile'), form=form)
+
+
